21948fc35a2408b7026ebee328b2ab3fb3b26078
Jake <jake.ravel92@gmail.com>
2022-07-04 09:15:29 +0100

    Added BookContext.js, Navbar.js and some styling in index.css

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,23 @@
+import React, { createContext, useState } from 'react';
+import uuid from 'uuid/v1';
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+  const [books, setBooks] = useState([
+    {title: 'title', author: 'author', id: 1}
+  ]);
+  const addBook = (title, author) => {
+    setBooks([...books, {title, author, id: uuid() }])
+  };
+  const removeBook = (id) => {
+    setBooks(books.filter(book => book.id !== id));
+  };
+  return (
+    <BookContext.Provider value={{books, addBook, removeBook}}>
+      { props.children }
+    </BookContext.Provider>
+  )
+}
+
+export default BookContextProvider;
